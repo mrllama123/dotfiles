2ad40b572df0b042be59721ecca8f25a1fffe3b8
Rowan Self <rowan.self@gmail.com>
2018-10-30 16:31:59 +1300

    added install script

---
+++ install_preq.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+sudo pacman -Syu
+
+# install preq
+sudo pacman -S firefox git yay code arc-gtk-theme jq zsh
+
+yay -S numix-icon-theme-git numix-circle-icon-theme-git 
+
+# download repo
+git clone https://github.com/unix121/i3wm-themer
+cd i3wm-themer/
+
+# install preq and setup for themer
+chmod u+x ./install_arch.sh
+./install_arch.sh
+sudo pip install -r requirements.txt
+cd src/
+sed -i  's+/v/+/bob/+g' config.yaml
+
+# backup config
+mkdir ~/theme-backups/one
+python i3wm-themer.py --config config.yaml --backup /home/bob/theme-backups/one
+
+#install theme
+python i3wm-themer.py --config config.yaml --install defaults
+
+python i3wm-themer.py --config config.yaml --load themes/000.json
+

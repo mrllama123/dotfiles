a4abfd91395701c9e537df4fafca8addc7b202e7
Rowan Self <rowan.self@gmail.com>
2018-11-12 15:12:01 +1300

    added profile and exspanded on installed programs

--- install_preq.sh
+++ install_preq.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 sudo pacman -Syu
 
-# install preq
-sudo pacman -S firefox yay code arc-gtk-theme jq termite light-locker
+# install apps
+sudo pacman -S firefox yay code jq termite git vim zsh powerline pacman
+
+# ui libarys
+sudo pacman -S qt5-base kvantum-manjaro kvantum-qt5 compton
+
+#i3
+sudo pacman -S pcmanfm-gtk3 i3-gaps i3exit powerline i3lock i3blocks lxappearance-gtk3 rofi
+
+# aur install
 yay -S numix-icon-theme-git numix-circle-icon-theme-git 
 
 #install oh my zsh
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-cp ~/.zshrc ~/.zshrc.orig
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-chsh -s /bin/zsh
+#git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+#cp ~/.zshrc ~/.zshrc.orig
+#cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+#chsh -s /bin/zsh
 
+# install spacevim
+#curl -sLf https://spacevim.org/install.sh | bash
 
 # install asdf
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.6.0
 echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.zshrc
 echo -e '\n. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
 
-sudo pacman -Rs palemoon-bin gimp 
 
 # download repo
 git clone https://github.com/unix121/i3wm-themer
@@ -26,16 +35,19 @@ cd i3wm-themer/
 # install preq and setup for themer
 chmod u+x ./install_arch.sh
 ./install_arch.sh
-sudo pip install -r requirements.txt
-cd src/
-sed -i  's+/v/+/bob/+g' config.yaml
 
-# backup config
-mkdir ~/theme-backups/one
-python i3wm-themer.py --config config.yaml --backup /home/bob/theme-backups/one
 
-#install theme
-python i3wm-themer.py --config config.yaml --install defaults
 
-python i3wm-themer.py --config config.yaml --load themes/000.json
+# sudo pip install -r requirements.txt
+# cd src/
+# sed -i  's+/v/+/bob/+g' config.yaml
+
+# # backup config
+# mkdir ~/theme-backups/one
+# python i3wm-themer.py --config config.yaml --backup /home/bob/theme-backups/one
+
+# #install theme
+# python i3wm-themer.py --config config.yaml --install defaults
+
+# python i3wm-themer.py --config config.yaml --load themes/000.json
 

7f8bcd4f8d484e6024d1b90bb900f5c7b0e33705
Rowan Self <mrllama123@users.noreply.github.com>
2023-02-03 11:31:27 +1300

    add setup script

---
+++ setup-comp-ubuntu.sh
@@ -0,0 +1,38 @@
+#! /usr/bin/bash
+sudo apt update && sudo apt dist-upgrade -y
+
+echo "-------- install core software --------"
+
+sudo apt install git curl wget gpg jq podman podman-docker zsh flatpak -y
+
+sudo apt-get install make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+systemctl --user enable --now podman.socket
+
+echo "-------- setup oh my zsh --------"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+echo "-------- setup asdf --------"
+
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
+
+echo "-------- setup vscode --------"
+
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+
+rm -f packages.microsoft.gpg
+
+sudo apt install apt-transport-https -y
+sudo apt update
+sudo apt install code
+
+
+
+
